fb7c5eb5aca69fc0c9029f29a38a46112fdf6c17
armzig <bellotaophyc@outlook.com>
2023-12-07 07:31:26 +0100

    configuring the cloud provider

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-2"
+}
+
+locals {
+  default_certs = var.use_default_domain ? ["default"] : []
+  acm_certs     = var.use_default_domain ? [] : ["acm"]
+  domain_name   = var.use_default_domain ? [] : [var.domain_name]
+}
+
